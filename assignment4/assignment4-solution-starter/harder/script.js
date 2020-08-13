e68a5979a9d9c1d39c571b1721b82087c56aacac
Rosan <67791289+roosan6@users.noreply.github.com>
2020-08-13 23:02:30 +0545

    Update script.js

--- assignment4/assignment4-solution-starter/harder/script.js
+++ assignment4/assignment4-solution-starter/harder/script.js
@@ -58,9 +58,9 @@ for (var i in names) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (firstLetter toLowerCase()==='j') {
-    byeSpeaker.speak(names[i]);
+  if (firstLetter === 'j') {
+     byeSpeaker.speak(names[i]);
   } else {
-    helloSpeaker.speak(names[i]);
+     helloSpeaker.speak(names[i]);
   }
 }
