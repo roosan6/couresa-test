e5c8807126c1872b826938b0976eea1ff446da0a
Rosan <67791289+roosan6@users.noreply.github.com>
2020-08-13 23:36:42 +0545

    Update script.js

--- assignment4/script.js
+++ assignment4/script.js
@@ -60,7 +60,7 @@ for (var i=0; i < names.length ; i++) {
   if (firstLetter toLowerCase()==='j') {
     byeSpeaker.speak(names[i]);
   } else {
-     helloSpeaker.speak(names[i]);
+    helloSpeaker.speak(names[i]);
   }
 }
 
